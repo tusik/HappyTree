8bcb0782e79a5e3be013ece4abe73feba5b65760
Image <onenookone@gmail.com>
2019-01-21 21:24:42 +0800

    feat(Interface):

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -1,14 +1,29 @@
+package Test;
+
 import cx.by.HappyTree.HappyTree;
+import cx.by.HappyTree.TreeNode;
+import cx.by.HappyTree.inter.Tree;
 import org.junit.Test;
 
 
 public class MainTest {
     @Test
     public void main(){
-        HappyTree happyTree = new HappyTree();
-        happyTree.insert("root");
-        HappyTree node1 = new HappyTree();
-        happyTree.add(node1);
-        System.out.println(happyTree);
+        HappyTree tree = new HappyTree();
+        TreeNode node1=new TreeNode();
+        TreeNode node2=new TreeNode();
+        TreeNode node3=new TreeNode();
+        TreeNode node1_1=new TreeNode();
+        TreeNode node1_2=new TreeNode();
+        TreeNode node1_3=new TreeNode();
+        TreeNode node1_1_1 = new TreeNode();
+        node1_1.addChild(node1_1_1);
+        node1.addChild(node1_1);
+        node1.addChild(node1_2);
+        node1.addChild(node1_3);
+        tree.addChild(node1);
+        tree.addChild(node2);
+        tree.addChild(node3);
+        System.out.println("final");
     }
 }
---
+++ src/cx/by/HappyTree/inter/Tree.java
@@ -0,0 +1,18 @@
+package cx.by.HappyTree.inter;
+
+import cx.by.HappyTree.HappyTree;
+import cx.by.HappyTree.TreeNode;
+
+public interface Tree {
+    HappyTree split(TreeNode node);
+    void initChild();
+    boolean addChild(Object node);
+    boolean removeChild(Object node);
+    boolean clear();
+    boolean insert(Object o);
+    Object contain();
+    boolean setParent(Object node);
+    void incrLength();
+    void reduceLength();
+    long length();
+}
